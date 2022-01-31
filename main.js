2abba8223410530fd6fd21675738d5171784ac6a
visual studio <adityashiroor@gmail.com>
2022-01-31 09:07:12 +0530

    Got rid of load-flicker

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-// just a test commit...
-// These are done before load, in order to reduce the amount of time we're on the 
-// wrong color mode, i.e. to reduce the page flash-time as the javascript loads.
 const BODY = document.body || document.querySelector("body");
 const IMG = BODY.querySelector(".color-switch img");
-const _DARK_MODE = localStorage.getItem("dark-mode");
-const  DARK_MODE = _DARK_MODE == null ? "true" : _DARK_MODE;
-// TODO: Somehow figure out how to temporarily disable transitions to load the user's stored theme. 
-// maybe we need server side logic to do this?
-if (DARK_MODE === "true") {
-    // set default color scheme only meant for webkit browsers, firefox is done in CSS
-    document.documentElement.style.colorScheme = "dark";
-
-} else {
-    // light theme
-    BODY.classList.remove("dark-mode");
+if (DARK_MODE !== "true") {
     IMG.src = "./resources/dark.svg";
 }
 const LOAD = () => 
@@ -35,24 +22,24 @@ const LOAD = () =>
     // switch color on click, and add an animation
     COLOR_SWITCH.addEventListener("click", () => {
         // change to dark mode
-        BODY.classList.toggle("dark-mode");
+        ROOT.classList.toggle("dark-mode");
         // spin-animate the color-switcher
         // https://stackoverflow.com/a/58353279 <- explains whatever hack this is 
         COLOR_SWITCH.classList.remove("rotate-animation");
         void COLOR_SWITCH.offsetWidth; // this hack
         COLOR_SWITCH.classList.add("rotate-animation");
         // change the image
-        if (BODY.classList.contains("dark-mode")) {
+        if (ROOT.classList.contains("dark-mode")) {
             localStorage.setItem("dark-mode", "true");
             IMG.src = "./resources/light.svg";
             // change to dark mode (only for webkit)
-            document.documentElement.style.colorScheme = "dark";
+            ROOT.style.colorScheme = "dark";
         }
         else {
             localStorage.setItem("dark-mode", "false");
             IMG.src = "./resources/dark.svg";
             // change to light mode (only for webkit)
-            document.documentElement.style.colorScheme = "light";
+            ROOT.style.colorScheme = "light";
         }
     });
 
